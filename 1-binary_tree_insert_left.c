20ff29384b193330d0c0d2fdbf2e67a216b73c5f
omar546 <omer.mkawy.om@gmail.com>
2024-01-05 22:14:41 +0200

    1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,12 +12,13 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+    binary_tree_t *node;
+
     if (!parent)
     {
 		return (NULL);
     }
 
-	binary_tree_t *node;
 	node = binary_tree_node(parent, value);
 
 	if (node == NULL)
