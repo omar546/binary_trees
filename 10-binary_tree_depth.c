28946dbc42febae8b03752fe91f057ef93fa737d
omar546 <omer.mkawy.om@gmail.com>
2024-01-05 23:14:47 +0200

    10

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - measures the depth of a node
+ *
+ * @tree: apointer to the node to measure
+ *
+ * Return: depth of the tree
+ *
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+size_t depth;
+
+if (!tree)
+{
+return (0);
+}
+
+if (tree->parent)
+{
+depth = 1 + binary_tree_depth(tree->parent);
+} else {
+depth = 0;
+}
+
+return (depth);
+}
